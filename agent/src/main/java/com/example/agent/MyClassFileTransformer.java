553c49b8220463e3329301c04c8fdc394f09ca2a
JackPeng123 <Jack.PENG@REInventVentures.com>
2021-09-16 16:47:14 +0800

    删除废代码

--- agent/src/main/java/com/example/agent/MyClassFileTransformer.java
+++ agent/src/main/java/com/example/agent/MyClassFileTransformer.java
@@ -1,11 +1,5 @@
 package com.example.agent;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
@@ -17,16 +11,7 @@ public class MyClassFileTransformer implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         if (className.contains("TestUser")) {
-            ClassReader classReader = new ClassReader(classfileBuffer);
-            ClassWriter classWriter = new ClassWriter(0);
-            ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM5) {
-                @Override
-                public void visitEnd() {
-                    super.visitEnd();
-                    //cv.visitMethod()
-
-                }
-            };
+            
         }
 
         return null;
